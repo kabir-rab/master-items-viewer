d242f858214b12004d611391c89fdcb32d77562a
kabir-rab <kabir-rab@users.noreply.github.com>
2020-02-29 23:46:34 +0000

    Added a angular service
    Major changes to the the way data is being fetched from the Qlik Engine. Using a angular service (which are singuar) rather than doing this in controler.

--- master-items-viewer.js
+++ master-items-viewer.js
@@ -1,16 +1,16 @@
-define( [
+define([
 	"text!./master-items-viewer-template.html",
 	"text!./lib/css/master-items-viewer.css",
 	"./lib/js/property",
 	"jquery",
 	"qlik",	
+	"./lib/js/masterItemService",
 	"./lib/js/table-resizer",
-	"./lib/js/jq-highlight"
+	"./lib/js/jq-highlight",
 ],
-
-function ( template, cssContent, prop, jQuery, qlik ) {
+function ( template, cssContent, prop, jQuery, qlik, myService ) {
 	'use strict';
-	$("<style>").html(cssContent).appendTo("head");	
+	$("<style>").html(cssContent).appendTo("head");
 	return {
 		definition: prop,
 		support: {
@@ -19,116 +19,16 @@ function ( template, cssContent, prop, jQuery, qlik ) {
 			snapshot: true
 		},
 		template: template,
-		controller: ['$scope', function( $scope ) {
-			var app = qlik.currApp(this),
+		controller: ['$scope', 'masterItemService', function( $scope, masterItemService ) {
+			var getMasterLibrary,
+				fetchData = masterItemService.getData().done(function(masterItems){
+					getMasterLibrary = function(callType){					
+						callType == 1 ? ($scope.measureDetails = masterItems.measures,createTagList($scope.measureDetails))
+										: ($scope.measureDetails = masterItems.dimensions,createTagList($scope.measureDetails));
+					};
+					getMasterLibrary(1);
+				}),
 				searchValue='',
-				nonMasterItemProp=[],
-				masterItemProp=[],				
-				dimensionList,
-				measureList,
-				// settings up the createGenericObject call for the qMeasureListDef
-				measureCall = {
-					qInfo: {
-					qId:"measureObjectExt",	
-					qType: "MeasureListExt"
-					},
-					qMeasureListDef: {
-					qType: "measure",
-					qData: {
-						title: "/qMetaDef/title",
-						tags: "/qMetaDef/tags",
-						expression: "/qMeasure/qDef",
-						description: "/qMetaDef/description"
-						}
-					}
-				},
-				// settings up the createGenericObject call for the qDimensionListDef
-				dimensionCall = {
-					qInfo: {
-					qId:"DimensionObjectExt",
-					qType: "DimensionListExt"
-					},
-					qDimensionListDef: {
-					qType: "dimension",
-					qData: {
-						grouping: "/qDim/qGrouping",
-						info: "/qDimInfos",
-						title: "/qMetaDef/title",
-						tags: "/qMetaDef/tags",
-						expression: "/qDimension/qDef",
-						description: "/qMetaDef/description"
-						}
-					}
-				},
-				// qDimensionListDef
-				generateDimensionList = app.createGenericObject(
-					dimensionCall, function(reply) {
-						dimensionList = reply.qDimensionList.qItems;
-				}),
-				// qMeasureListDef
-				generateMeasureList = app.createGenericObject(
-					measureCall, function(reply) {
-						measureList = reply.qMeasureList.qItems;
-				}),
-				// Starting loop to fetch all sheets id > then extended children property
-				appObjectsProperty = app.getList( "Sheet", function(reply){	
-					var sheetCounter = 0,
-						objectCounter = 0;
-					$.each(reply.qAppObjectList.qItems, function(key, value) {
-						app.getFullPropertyTree(value.qInfo.qId).then(function(model){
-							sheetCounter ++;
-							objectCounter = 0;
-							$.each(model.propertyTree.qChildren, function(key, value) {
-								objectCounter ++;
-								let parent = sheetCounter == reply.qAppObjectList.qItems.length;
-								let clild = objectCounter == model.propertyTree.qChildren.length;								
-								if(value.qProperty.qExtendsId){
-									masterItemProp.push(value);
-								}
-								else{
-									nonMasterItemProp.push(JSON.stringify(value));
-								};
-								if (parent && clild) {
-									//console.log("nonMasterItemProp",nonMasterItemProp);
-									$.each(dimensionList, function(key, dValue){										
-										var holdingObject = [];										
-										$.each(nonMasterItemProp, function(key, value) {
-											if(value.indexOf('"qId":"') != -1){
-												var idStartPosition = value.indexOf('"qId":"')+7;
-												var idFinishPosition = value.indexOf('",', idStartPosition);
-												var holdingObjectId = value.substring(idStartPosition, idFinishPosition);
-											}
-											if(value.indexOf('"'+dValue.qInfo.qId+'"') > -1){												
-												if($.inArray(holdingObjectId, holdingObject) == -1){
-													holdingObject.push(holdingObjectId);
-												}												
-											}
-										});										
-										dValue.qInfo.inobject = holdingObject;
-									});
-									$.each(measureList, function(key, dValue){
-										var holdingObject = [];										
-										$.each(nonMasterItemProp, function(key, value) {
-											if(value.indexOf('"qId":"') != -1){
-												var idStartPosition = value.indexOf('"qId":"')+7;
-												var idFinishPosition = value.indexOf('",', idStartPosition);
-												var holdingObjectId = value.substring(idStartPosition, idFinishPosition);
-											}
-											if(value.indexOf('"'+dValue.qInfo.qId+'"') > -1){												
-												if($.inArray(holdingObjectId, holdingObject) == -1){
-													holdingObject.push(holdingObjectId);
-												}
-											}
-										});										
-										dValue.qInfo.inobject = holdingObject;
-										//console.log(dValue);
-									});
-									var firstDisplay = getMasterLibrary(1);
-								};
-							});
-						});
-					});
-				}),
 				// create list of all tags with unique values
 				createTagList = function(list){
 					var tagList=[];
@@ -142,13 +42,8 @@ function ( template, cssContent, prop, jQuery, qlik ) {
 						};
 					});
 					$scope.tags = tagList;
-				},
-				// function to create generic object for Master Dimensions and Master Measures
-				getMasterLibrary = function(callType){					
-					callType == 1 ? ($scope.measureDetails = measureList, createTagList($scope.measureDetails))
-									: ($scope.measureDetails = dimensionList,createTagList($scope.measureDetails));
-				};	
-						
+				};
+
 			$scope.measureCss = 'lui-active'
 			// function to change views (dimensions or measures)
 			$scope.viewChange = function(viewNo){
@@ -164,7 +59,7 @@ function ( template, cssContent, prop, jQuery, qlik ) {
 			};
 			// clear search function
 			$scope.searchTagAndClear = function (tag) {
-				if(tag.length > 0){		
+				if(tag.length > 0){
 					$scope.searchText = tag;
 					$scope.search();
 				}
@@ -180,7 +75,7 @@ function ( template, cssContent, prop, jQuery, qlik ) {
 			$scope.sortBy = function(propertyName) {
 				$scope.reverse = ($scope.propertyName === propertyName) ? !$scope.reverse : false;
 				$scope.propertyName = propertyName;
-			};								
+			};
 		}],
 		paint: function ($element, layout) {
 			// set hight for the scroll bar
